4537c4dd51fee08625ae6f60c956155a2c784b37
caffedrine <bacchusstyle@yahoo.com>
2018-11-06 09:15:30 +0200

    sophia v3 CPP code to be added

---
+++ v3/RPiFirmware/cmake-build-debug/CMakeFiles/RPiFirmware.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPiFirmware.dir/main.cpp.o"
+  "RPiFirmware.pdb"
+  "RPiFirmware"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPiFirmware.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ v3/RPiFirmware/cmake-build-debug/CMakeFiles/RPiFirmware.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RPiFirmware.
+# This may be replaced when dependencies are built.
